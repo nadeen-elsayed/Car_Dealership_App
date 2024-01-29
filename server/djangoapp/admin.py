47a51f4acfa3a909fa1f7b156f04f9e1b90614fc
nadeen-elsayed <dodynadeen0@gmail.com>
2024-01-29 13:00:45 -0500

    Build Car Model and Car Make models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -6,9 +6,15 @@ from .models import CarMake, CarModel
 admin.site.register(CarMake)
 admin.site.register(CarModel)
 # CarModelInline class
-
+class CarModelInline(admin.StackedInline):
+    model = CarModel 
+    extra = 5
 # CarModelAdmin class
-
+class CourseAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'name', 'description']
+    
 # CarMakeAdmin class with CarModelInline
-
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ['dealer_id', 'name', 'car_type', 'year']
+    inlines = [CarModelInline]
 # Register models here
